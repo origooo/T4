8ef151969fe382e4da3f64dd53278e56d9c01902
William Granli <william.granli@gmail.com>
2014-01-23 15:25:08 +0100

    Added my Assignment 1

---
+++ William/Exercise 1/Exerc_1_1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+main()
+{
+	int input;
+	
+	printf("Enter a number between 1 and 10! \n");
+	scanf("%d", &input);
+	
+	switch (input) {
+   		case 1:
+			printf("You entered 1");
+			break;
+  		case 2:
+			printf("You entered 2");
+			break;
+  		case 3:
+			printf("You entered 3");
+			break;
+  		case 4:
+			printf("You entered 4");
+			break;
+  		case 5:
+			printf("You entered 5");
+			break;
+  		case 6:
+			printf("You entered 6");
+			break;
+  		case 7:
+			printf("You entered 7");
+			break;
+  		case 8:
+			printf("You entered 8");
+			break;
+  		case 9:
+			printf("You entered 9");
+			break;
+		case 10:
+			printf("You entered 10");
+			break;
+	}
+}
