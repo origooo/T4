81efacb6e380aacb837ea79d7ba6babf8d13a2bf
Patrik Bäckström <mail@patrikbackstrom.com>
2014-01-23 15:14:12 +0100

    Added exercise_2_1

--- Patrik/exerc_1_4.c
+++ Patrik/exerc_1_4.c
@@ -1,5 +1,5 @@
 /*====================================
-File name: exerc_1_4.c (or cpp) Date: 2014‐01‐22
+File name: exerc_1_4.c (or cpp) Date: 2014‐01‐23
 Group Number: 3
 Members that contributed:
 	Patrik Bäckström
@@ -13,27 +13,56 @@ Demonstration code: [<Ass code 1‐4> <abc>]
 
 #define MAX_GUESS 100
 #define MIN_GUESS 1
+#define MAX_NUMBER_OF_GUESSES 5
+
+void clearstdin(void);
+
+int main(int argc, char *args[]) {
+
+	int guess, guesses, success=0;
+
+playAgain:
+
+	srand(time(NULL));
+	int correctGuess = rand() % 100 + 1;
+	guesses = 0;
+	//printf("\n%i\n", correctGuess);
+	printf("Guess a number from 1 to 100: ");
+
+	do {
+		scanf("%d", &guess);
+		clearstdin();
+		if (guess >= MIN_GUESS && guess <= MAX_GUESS) {
+			if (guess == correctGuess) {
+				success = 1;
+				goto breakGameLoop;
+			} else if (guess != correctGuess && guesses == MAX_NUMBER_OF_GUESSES-1)
+				goto breakGameLoop;
+			else if (guess < correctGuess)
+				printf("%i is to low! Guess again: ", guess);
+			else if (guess > correctGuess)
+				printf("%i is to high! Guess again: ", guess);
+		} else {
+			printf("That's not a proper guess... Guess again: ");
+		}
+		guesses++;
+
+	} while (guess != correctGuess);
+
+breakGameLoop:
+	if (success==1)
+		printf("\n%i is correct, and you guessed %i/%d times! Congradulations, MF!\n\n", guess, guesses, MAX_NUMBER_OF_GUESSES);
+	else
+		printf("\nYou're out of guesses!\n\n");
+
+	printf("Play again (y/n): ");
+	if (getchar() == 'y')
+		goto playAgain;
+
+	return 0;
+}
 
 void clearstdin(void) {
 	char temp;
 	while((temp=getchar())!='\n');
-}
-
-int main(int argc, char *args[]) {
-
-	int guess = -1;
-
-	srand(time(NULL));
-	int correctGuess = rand() % 100 + 1;
-	printf("\n%i\n", correctGuess);
-	printf("Guess a number from 1 to 100: ");
-
-	clearstdin();
-	scanf("%d", &guess);
-
-	if (isdigit(guess))
-		printf("Guess: %i\n", guess);
-
-
-	return 0;
 }
