fd6d33c81a3359fc55878c3b69b51d46aa86e40f
Patrik Bäckström <mail@patrikbackstrom.com>
2014-01-24 14:06:54 +0100

    Added exerc_2_3.c

---
+++ Patrik/exerc_2_3.c
@@ -0,0 +1,9 @@
+/*====================================
+File name: exerc_2_3.c (or cpp) Date: 2014‐01‐24
+Group Number: 3
+Members that contributed:
+	Patrik Bäckström,
+	John Burchell,
+	William Granli
+Demonstration code: [<Ass code 1‐4> <abc>]
+======================================*/
