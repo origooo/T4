6dc9351eee2f4c42459fbc71cd691da2cf2c5e5a
Patrik Bäckström <mail@patrikbackstrom.com>
2014-01-24 22:59:14 +0100

    Updated completed exerc_2_3.c

--- Patrik/exerc_2_3.c
+++ Patrik/exerc_2_3.c
@@ -7,3 +7,49 @@ Members that contributed:
 	William Granli
 Demonstration code: [<Ass code 1‐4> <abc>]
 ======================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+
+int cmpWithout(char *, char *);
+
+int main(int argc, char *argv[]) {
+
+	if (argc != 3) {
+		printf("\n\t# Not correct amount of arguments...\n\tExactly two (2) is required!\n\n\tExiting...\n\n");
+	} else {
+		printf("\n\t# Comparing arguments WITHOUT strcmp(): ");
+		if (cmpWithout(argv[1],argv[2]))
+			printf("\tStrings are equal\n");
+		else
+			printf("\tStrings are NOT equal\n");
+
+		printf("\t# Comparing arguments WITH strcmp(): ");
+		if (!strcmp(argv[1],argv[2]))
+			printf("\t\tStrings are equal\n");
+		else
+			printf("\t\tStrings are NOT equal\n");
+		printf("\n");
+	}
+
+	return 1337;
+}
+
+int cmpWithout(char *str1, char *str2) {
+
+	if (strlen(str1) != strlen(str2))
+		return 0;
+	
+	int i = 0;
+	while (*(str1 + i) != '\0') {
+		if (*(str1 + i) == *(str2 + i)) {
+			i += sizeof(char);
+			continue;
+		} else 
+			return 0;
+	}
+	return 1;
+}
