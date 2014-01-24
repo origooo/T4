8db16123a0ef88a576e73a3efbc2ead763260d65
Patrik Bäckström <mail@patrikbackstrom.com>
2014-01-24 11:18:53 +0100

    Updated exerc_2_1

--- Patrik/exerc_2_1.c
+++ Patrik/exerc_2_1.c
@@ -12,35 +12,69 @@ Demonstration code: [<Ass code 1‐4> <abc>]
 #include <stdlib.h>
 #include <string.h>
 
-#define TEXT_LENGTH 30
+#define TEXT_LENGTH 100
 
+void readFile();
 void without(char *str1);
 void with(char *str1);
+void clearstdin(void);
 
 int main(int argc, char *argv[]) {
 
 	char str1[TEXT_LENGTH];
 
-	printf("\nInput string (max length: %i): ",TEXT_LENGTH);
+	if (argv[0] == NULL)
+		printf("\nInput string (max length: %i): \n",TEXT_LENGTH);
+	else {
+		printf("\nArgument found...");
+		printf("\nReading string from text file (max length: %i): \n", TEXT_LENGTH);
+	}
 	fgets(str1, TEXT_LENGTH, stdin);
 
 	without(str1);
 
-	printf("\nInput another string (max length: %i): ",TEXT_LENGTH);
+	if (argv[0] == NULL)
+		printf("\nInput string (max length: %i): \n",TEXT_LENGTH);
+	else {
+		printf("\nArgument found...");
+		printf("\nReading string from text file (max length: %i): \n", TEXT_LENGTH);
+	}
 	fgets(str1, TEXT_LENGTH, stdin);
+
 	with(str1);
+	//readFile(argv[0]);
 
 	return 0;
+}
+
+void readFile(char *filename) {
+
+	FILE *file;
+	//char *filename = "/tmp/foo";
+	file = fopen(filename, "r");
+	char line[TEXT_LENGTH];
+	if (file == NULL) {
+		fprintf(stderr, "File %s could not be opened\n", filename);
+		exit(1);
+	}
+	fgets(line, TEXT_LENGTH,file);
+	fclose(file);
+	printf("\nString from text file: %s\n\n",line);
 }
 
 void without(char *str1) {
 
 	char copy[TEXT_LENGTH];
-	for (int i = 0; i < sizeof(str1); i++) {
+	int i;
+	while (i < TEXT_LENGTH && str1[i] != '\n') {
 		copy[i] = str1[i];
+		i++;
 	}
+	//for (int i = 0; i < sizeof(str1); i++) {
+	//	copy[i] = str1[i];
+	//}
 
-	printf("Copying w/o strcpy():\n");
+	printf("### Copying w/o strcpy().\n");
 	printf("\tOriginal: %s", str1);
 	printf("\tCopy: %s", copy);
 }
@@ -50,7 +84,12 @@ void with(char *str1) {
 	char copy[TEXT_LENGTH];
 	strcpy(copy, str1);
 
-	printf("Copying with strcpy():\n");
+	printf("### Copying with strcpy().\n");
 	printf("\tOriginal: %s", str1);
 	printf("\tCopy: %s", copy);
+}
+
+void clearstdin(void) {
+	char temp;
+	while((temp=getchar())!='\n');
 }
